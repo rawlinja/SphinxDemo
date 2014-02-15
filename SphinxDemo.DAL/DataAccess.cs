daad18ea9761837d4b45fda0a59f850581477f34
Jermaine Rawlins <jermaine.rawlins@gmail.com>
2014-02-14 22:46:01 -0500

    Initial import of SphinxDemo

---
+++ SphinxDemo.DAL/DataAccess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+namespace SphinxDemo.DAL
+{
+    public class CityModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CountryCode { get; set; }
+        public string District { get; set; }
+        public int Population { get; set; }
+    }
+
+    public class SphinxDataAccess
+    {
+        public static List<CityModel> GetCityData()
+        {
+            List<CityModel> results;
+            const string connectionString = "Server=localhost; Port=9303";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                results = connection.Query<CityModel>("SELECT * FROM city").ToList();
+            }
+
+            return results;
+        }
+    }
+}
---
+++ SphinxDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SphinxDemo.MvcApplication" Language="C#" %>
